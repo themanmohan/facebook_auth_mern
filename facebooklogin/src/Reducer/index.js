a3c4aa7ee0cdd0db493a1e992c36a85e734fac53
Manmohan Kumar <manmohankumar023@gmail.com>
2021-07-30 08:27:08 +0530

    delete

--- facebooklogin/src/Reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import FeedBack from './FeedBack'
-import auth from './auth'
-
-//combinig reducer
-export default combineReducers({
-    FeedBack,
-    auth
-})
